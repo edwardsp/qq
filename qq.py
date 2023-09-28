88415306354deb12bdaf94d0721b040b4cf78b0e
Nico Erfurth <nico.erfurth@microsoft.com>
2023-09-28 18:53:57 +0200

    Added support for OpenAI direct endpoint
    Added support for gpt4 models
    General streamlining of code
    Don't set the engine, but the model
    Generic config file search

--- qq.py
+++ qq.py
@@ -11,6 +11,7 @@ import psutil
 import pyperclip
 import sqlite3
 import sys
+import functools
 
 logger = logging.getLogger('qq')
 
@@ -41,6 +42,16 @@ def error_and_exit(message):
 
 system_prompt = "You are a tool designed to help users run commands in the terminal. Only use the functions you have been provided with.  Do not include the command to run the shell unless it is different to the one running."
 system_prompt_verbose = "You are an assistant for users running commands in the terminal.  Answer with just the simple shell instructions and provide an explanation."
+
+# List of models that are supported, taken from the /v1/chat/completions endpoint list
+# https://platform.openai.com/docs/models/model-endpoint-compatibility
+# Removed all snapshot versions, first version is used as the default
+supported_models = [
+    "gpt-3.5-turbo",
+    "gpt-4",
+    "gpt-3.5-turbo-16k",
+    "gpt-4-32k",
+]
 
 def setup_database():
     conn.execute('''CREATE TABLE IF NOT EXISTS history (
@@ -111,7 +122,7 @@ def openai_chat_completion(model, prompt, question, functions, function_call, te
     logger.debug(f"model: {model}")
     logger.debug(f"question: {question}")
     func_args = {
-        "engine": model,
+        "model": model,
         "messages": [
             {"role": "system", "content": prompt},
             {"role": "user", "content": question}
@@ -133,22 +144,22 @@ def openai_chat_completion(model, prompt, question, functions, function_call, te
         logger.error(f"OpenAI API returned an Authentication Error: {e}")
         sys.exit(1)
     except openai.error.APIConnectionError as e:
-        logger.error(f"Failed to connect to OpenAI API: {e}")
+        logger.error("Failed to connect to OpenAI API: {e}")
         sys.exit(1)
     except openai.error.InvalidRequestError as e:
-        logger.error(f"Invalid Request Error: {e}")
+        logger.error("Invalid Request Error: {e}")
         sys.exit(1)
     except openai.error.RateLimitError as e:
-        logger.error(f"OpenAI API request exceeded rate limit: {e}")
+        logger.error("OpenAI API request exceeded rate limit: {e}")
         sys.exit(1)
     except openai.error.ServiceUnavailableError as e:
-        logger.error(f"Service Unavailable: {e}")
+        logger.error("Service Unavailable: {e}")
         sys.exit(1)
     except openai.error.Timeout as e:
-        logger.error(f"Request timed out: {e}")
+        logger.error("Request timed out: {e}")
         sys.exit(1)
     except:
-        logger.exception(f"An unknown exception has occurred")
+        logger.exception("An unknown exception has occurred")
         sys.exit(1)
 
 def ask_chat_completion_question(model, question, temperature):
@@ -179,23 +190,21 @@ def ask_chat_completion_question(model, question, temperature):
 
     answer = openai_chat_completion(model, prompt, question, functions, function_call, temperature)
     
-    if 'function_call' in answer:
-        if answer['function_call']['name'] != 'run_command':
-            logger.error(f"Invalid function requested: {answer['function_call']['name']}")
-            sys.exit(1)
-        try:
-            args = json.loads(answer['function_call']['arguments'])
-        except:
-            logger.error(f"Invalid JSON arguments returned from the function API - {answer['function_call']['arguments']}")
-            sys.exit(1)
-        if 'command' not in args:
-            logger.error("Missing command argument in run_command function call.")
-            sys.exit(1)
-        return args['command']
+    if 'function_call' not in answer:
+        logger.debug(answer)
+        error_and_exit("Cannot process the response, missing function_call.")
 
-    logger.debug(answer)
-    logger.error("Cannot process the response.")
-    sys.exit(1)
+    if answer['function_call']['name'] != 'run_command':
+        error_and_exit(f"Invalid function requested: {answer['function_call']['name']}")
+    try:
+        args = json.loads(answer['function_call']['arguments'])
+    except Exception as e:
+        logger.exception(f"Invalid JSON arguments returned from the function API - {answer['function_call']['arguments']}\n{e}")
+        sys.exit(1)
+    if 'command' not in args:
+        error_and_exit("Missing command argument in run_command function call.")
+    return args['command']
+
     
 def ask_chat_completion_explanation(model, question, answer, temperature):
     detected_os = detect_os()
@@ -219,43 +228,61 @@ def ask_chat_completion_explanation(model, question, answer, temperature):
     logger.error("Cannot process the response.")
     sys.exit(1)
 
-def find_config():
-    # look for `config.json` in the current directory first otherwise ~/.qq_config.json
-    config_file = os.path.join(os.getcwd(), 'config.json')
-    logger.debug("Looking for config file: {config_file}")
-    if os.path.exists(config_file):
-        logger.info("Using config file: {config_file}")
-        return config_file
-    
-    config_file = os.path.join(os.path.expanduser("~"), '.qq_config.json')
-    logger.debug("Looking for config file: {config_file}")
-    if os.path.exists(config_file):
-        logger.info("Using config file: {config_file}")
-        return config_file
-    
+@functools.cache
+def load_configfile() -> dict[str, str]:
+    """
+    Look for `config.json` in the current directory first otherwise ~/.qq_config.json
+    """
+    locations = (
+        os.path.join(os.getcwd(), 'config.json'),
+        os.path.join(os.path.expanduser("~"), '.qq_config.json')
+    )
+    try:
+        for config_file_name in locations:
+            logger.debug("Looking for config file: {config_file_name}")
+            if not os.path.exists(config_file_name):
+                continue
+            logger.info("Using config file: {config_file_name}")
+            with open(config_file_name) as config_file:
+                config_details = json.load(config_file)
+                return config_details
+    except Exception as e:
+        logger.exception(f"Failed to load config file")
+        sys.exit(1)
+
     logger.error("No config file found.")
     sys.exit(1)
+
+@functools.cache
+def get_config_value(config_name: str, default_value = None):
+    config_data = load_configfile()
+
+    # Check environment variables first, then config file, last return default value
+    config_value = os.getenv(config_name)
+    if config_value:
+        return config_value
+
+    config_value = config_data.get(config_name, default_value)
+    if config_value:
+        return config_value
+    error_and_exit(f"Config value {config_name} not found.")
 
 def quickquestion():
     setup_database()
 
-    config_filename = find_config()
-    # Load config values
-    with open(config_filename) as config_file:
-        config_details = json.load(config_file)
-    
-    openai.api_type = "azure"
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    openai.api_base = config_details['OPENAI_API_BASE']
-    openai.api_version = config_details['OPENAI_API_VERSION']
+    # Defaulting to Azure for backwards compatibility, set to open_ai for the OpenAI API
+    openai.api_type = get_config_value('OPENAI_API_TYPE', 'azure').lower()
+    openai.api_key = get_config_value("OPENAI_API_KEY")
 
-    model_choices = []
-    if 'OPENAI_GPT35TURBO_MODEL' in config_details:
-        model_choices.append('gpt35turbo')
+    if openai.api_type == 'azure':
+        openai.api_base = get_config_value('OPENAI_API_BASE')
+        openai.api_version = get_config_value('OPENAI_API_VERSION')
+    elif openai.api_type == 'open_ai':
+        openai.organization = get_config_value('OPENAI_ORGANIZATION')
 
-    if len(model_choices) == 0:
-        print("No models are configured. Please add a model to your config.json file.")
-        exit(1)
+    default_model = get_config_value('OPENAI_MODEL', supported_models[0])
+    if default_model not in supported_models:
+        logger.warning(f"Configured model {default_model} is not in the list of supported models.")
 
     parser = argparse.ArgumentParser(description='Ask a quick question from the terminal')
     parser.add_argument(
@@ -266,7 +293,7 @@ def quickquestion():
         help="Set the logging verbosity level (default: INFO)",
     )
     parser.add_argument('--explain', '-e', help='Give an explanation for the command.  Either leave blank for the previous command or use the index from the history command.', type=int, default=0, nargs='?')
-    parser.add_argument('--model', '-m', choices=model_choices, default=model_choices[0], help='Choose a model')
+    parser.add_argument('--model', '-m', choices=supported_models, default=default_model, help='Choose a model')
     parser.add_argument('--temperature', '-t', help='Set the temperature for the AI model', default=0.0, type=float)
     parser.add_argument('--history', help='Show the history of commands and responses', action='store_true')
     parser.add_argument('question', nargs='*', help='The question to ask')
@@ -278,38 +305,23 @@ def quickquestion():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
-    logger.debug(f"Config file: {config_filename}")
-    logger.debug(f"Config details: {config_details}")
-
     if args.history:
         print(get_history())
         exit(0)
 
-    explain = (args.explain != 0)
-
-    if explain:
+    if args.explain != 0:
         q, a = get_history_item(args.explain)
+        print("Question: ", q)
+        print("Answer: ", a)
+        print("Explanation:")
+        a = ask_chat_completion_explanation(args.model, q, a, args.temperature)
     else:
         q = ' '.join(args.question)
-
-    if args.model == 'gpt35turbo':
-        model = config_details['OPENAI_GPT35TURBO_MODEL']
-        if explain:
-            print("Question: ", q)
-            print("Answer: ", a)
-            print("Explanation:")
-            a = ask_chat_completion_explanation(model, q, a, args.temperature)
-        else:
-            a = ask_chat_completion_question(model, q, args.temperature)
-    else:
-        logger.error(f"Unknown model: {args.model}")
-        exit(1)
-
-    print(a)
-
-    if not explain:
+        a = ask_chat_completion_question(args.model, q, args.temperature)
         pyperclip.copy(a)
         append_to_history(q, a)
+
+    print(a)
 
 if __name__ == "__main__":
     quickquestion()
